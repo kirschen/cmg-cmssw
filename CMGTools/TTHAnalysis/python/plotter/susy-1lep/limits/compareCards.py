1098df117037f4077a8eb1efd8f3180e69cb7d83
Henning Kirschenmann <henning.kirschenmann@cern.ch>
2015-06-16 11:42:02 +0200

    enhancing compareCards script

--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/limits/compareCards.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/limits/compareCards.py
@@ -2,6 +2,7 @@
 from copy import deepcopy
 from tabulate import tabulate
 from collections import OrderedDict
+import itertools
 
 import re
 from sys import argv
@@ -21,6 +22,7 @@ parser.add_option("--ic", "--include-channel", type="string", dest="channelInclu
 parser.add_option("--X-no-jmax",  dest="noJMax", default=False, action="store_true", help="FOR DEBUG ONLY: Turn off the consistency check between jmax and number of processes.")
 parser.add_option("--xn-file", "--exclude-nuisances-from-file", type="string", dest="nuisVetoFile", help="Exclude all the nuisances in this file")
 parser.add_option("--en-file", "--edit-nuisances-from-file", type="string", dest="editNuisFile", help="edit the nuisances in this file")
+parser.add_option("--TableType", type="string", dest="tableType", default="BkgEst",help="choose the table type")
 
 (options, args) = parser.parse_args()
 options.bin = True # fake that is a binary output, so that we parse shape lines
@@ -37,82 +39,146 @@ keyline = []; expline = []; systlines = {}
 signals = []; backgrounds = []; shapeLines = []
 cmax = 5 # column width
 
-DC_Sig = Datacard()
-DC_Bkg = Datacard()
+
+if options.tableType=="BkgEst":
+    
+    DC_Sig = Datacard()
+    DC_Bkg = Datacard()
+    
+    
+    
+    if not args:
+        raise RuntimeError, "No input datacards specified."
+    for ich,fname in enumerate(args):
+        label = "ch%d" % (ich+1)
+        if "=" in fname: (label,fname) = fname.split("=")
+        fname = options.fprefix+fname
+        dirname = os.path.dirname(fname)
+        file = open(fname, "r")
+        DC = parseCard(file, options)
+        if label == "Bkg": DC_Bkg = DC #parseCard(file, options) #deepcopy(DC)
+        elif label == "Sig": DC_Sig = DC #parseCard(file, options)
+            
+    
+    bins = []
+    
+    
+    #for b in DC_Bkg.bins:
+    CollectYieldDicts = []
+    CollectYieldCompDicts = []
+    
+    for b in DC_Bkg.bins:
+        if "_1p_" in b:
+            YieldDict = OrderedDict( [ ("Bin",b), ("CR",0), ("CR-Sig.Cont.",0), ("CR-DiLep.Pur.",0), #("1B", 0), ("1BRel", 0), 
+                                       ("2B", 0), ("2BRel", 0), ("2BBkgFrac", 0), ("3p", 0), ("3pRel", 0), ("3pBkgFrac", 0)])
+            YieldDictComp = OrderedDict( [ ("Bin",b)])
+            for (p,e) in DC_Bkg.exp[b].items(): # so that we get only self.DC.processes contributing to this bin
+                if "DiLep" in p:
+                    YieldDict["CR"] += e
+                    
+    
+            for (p,e) in DC_Bkg.exp[b].items(): # so that we get only self.DC.processes contributing to this bin
+                if DC_Bkg.isSignal[p]:
+                    YieldDict["CR-Sig.Cont."]= e/YieldDict["CR"] if YieldDict["CR"]!=0 else -1
+    
+            if DC_Bkg.obs[b]!=0: YieldDict["CR-DiLep.Pur."]= YieldDict["CR"]/DC_Bkg.obs[b]
+            else: YieldDict["CR-DiLep.Pur."]= -1
+            for repl in [#"_1B_",
+                "_2B_","_3p_"]:
+    #            replacedBin = b.replace("_1p_",repl).replace("DPhi00","DPhi10")
+                replacedBin = b.replace("_1p_",repl).replace("DPhi00","DPhi05")
+    #            replacedBin = b.replace("_1p_",repl).replace("DPhi00","Stop")
+    #            replacedBin = b.replace("_1p_",repl).replace("DPhi00","Top")
+                if replacedBin in DC_Sig.list_of_bins():
+                    for (p,e) in DC_Sig.exp[replacedBin].items(): # so that we get only self.DC.processes contributing to this bin
+                        if "DiLep" in p:
+                            YieldDict[repl.replace("_","")] += e
+                            YieldDict[repl.replace("_","")+"Rel"] = YieldDict["CR"]/e
+                            YieldDict[repl.replace("_","")+"BkgFrac"] = e/DC_Sig.obs[replacedBin]
+                            YieldDictComp["SR_"+p+"_"+repl.replace("_","")] = e
+                            
+                            
+    
+                else:
+                    YieldDict[repl.replace("_","")] = -1
+                    YieldDict[repl.replace("_","")+"Rel"] = -1
+    
+    
+            for repl in [#"_1B_",
+                "_2B_","_3p_"]:
+                replacedBin = b.replace("_1p_",repl).replace("DPhi00","DPhi05")
+                if replacedBin in DC_Sig.list_of_bins():
+                    for (p,e) in DC_Sig.exp[replacedBin].items(): # so that we get only self.DC.processes contributing to this bin
+                        if "DiLep" in p:
+    #                        print p
+                            YieldDictComp["SR_"+p+"_"+repl.replace("_","")] = YieldDictComp["SR_"+p+"_"+repl.replace("_","")]/YieldDict[repl.replace("_","")]
+    
+    
+            CollectYieldDicts.append(YieldDict)
+            CollectYieldCompDicts.append(YieldDictComp)
+    print 
+    print tabulate(CollectYieldDicts, headers="keys", floatfmt=".4f")
+    print tabulate(CollectYieldDicts, headers="keys", floatfmt=".4f", tablefmt="latex")
+    print tabulate(CollectYieldCompDicts, headers="keys", floatfmt=".4f", tablefmt="latex")
+
+if options.tableType=="BinByBinYields":
+    
+
+    
+    
+
+    SignalDict={}
+    SignalConfDict={}
+    
+    
+    if not args:
+        raise RuntimeError, "No input datacards specified."
+    for ich,fname in enumerate(args):
+        label = "ch%d" % (ich+1)
+        if "=" in fname: (label,STString,RDString,fname) = fname.split("=")
+        fname = options.fprefix+fname
+        dirname = os.path.dirname(fname)
+        file = open(fname, "r")
+        DC = parseCard(file, options)
+        SignalDict[label]=DC
+        SignalConfDict[(label,"STString")]=STString
+        SignalConfDict[(label,"RDString")]=RDString
+            
+    
+    bins = []
+    print SignalDict
+
+    CollectYieldDicts = []
+#    print SignalDict.keys()[0], SignalDict[SignalDict.keys()[0]], SignalDict[SignalDict.keys()[0]].bins
+    FirstEntryKey=SignalDict.keys()[0]
+    for b in SignalDict[FirstEntryKey].bins:
+#        print b
+        BinsToSelectAny=["_1B_","_2B_","_3p_"]
+        BinsToSelectAll=["CnC2015X"] #e.g. ,"9Infj"] to select only highest bins
+        if any(x in b for x in BinsToSelectAny) and all(x in b for x in BinsToSelectAll):
+#            print [ [(key+"Bkg",0), (key+"Sig",0)] for key in SignalDict ]
+
+#            print [ ("Bin",b)] + [ [(label+"Bkg",0), (label+"Sig",0)] for key in SignalDict]
+            YieldDict = OrderedDict( [ ("Bin",b)] + list(itertools.chain.from_iterable([ [(key+"Bkg",0), (key+"Sig",0), (key+"SovB",0)] for key in SignalDict ])) )
+#            print YieldDict
+
+            for key in SignalDict:
+                renamedBin = b.replace(SignalConfDict[(FirstEntryKey,"STString")],SignalConfDict[(key,"STString")])
+                renamedBin = renamedBin.replace(SignalConfDict[(FirstEntryKey,"RDString")],SignalConfDict[(key,"RDString")])
+#                print key, renamedBin, SignalDict[key].obs[renamedBin]
+                YieldDict[key+"Bkg"]=SignalDict[key].obs[renamedBin]
+
+                for (p,e) in SignalDict[key].exp[renamedBin].items(): # so that we get only self.DC.processes contributing to this bin
+                    if SignalDict[key].isSignal[p]:
+                        YieldDict[key+"Sig"]= e
+                        if YieldDict[key+"Bkg"]!=0:
+                            YieldDict[key+"SovB"]= e/YieldDict[key+"Bkg"]
 
 
+            #DC_Bkg.obs[b]           
+#            YieldDict[]=DC_Bkg.obs[b]
 
-if not args:
-    raise RuntimeError, "No input datacards specified."
-for ich,fname in enumerate(args):
-    label = "ch%d" % (ich+1)
-    if "=" in fname: (label,fname) = fname.split("=")
-    fname = options.fprefix+fname
-    dirname = os.path.dirname(fname)
-    file = open(fname, "r")
-    DC = parseCard(file, options)
-    if label == "Bkg": DC_Bkg = DC #parseCard(file, options) #deepcopy(DC)
-    elif label == "Sig": DC_Sig = DC #parseCard(file, options)
-        
-
-bins = []
+            CollectYieldDicts.append(YieldDict)
 
 
-#for b in DC_Bkg.bins:
-CollectYieldDicts = []
-CollectYieldCompDicts = []
-
-for b in DC_Bkg.bins:
-    if "_1p_" in b:
-        YieldDict = OrderedDict( [ ("Bin",b), ("CR",0), ("CR-Sig.Cont.",0), ("CR-DiLep.Pur.",0), #("1B", 0), ("1BRel", 0), 
-                                   ("2B", 0), ("2BRel", 0), ("2BBkgFrac", 0), ("3p", 0), ("3pRel", 0), ("3pBkgFrac", 0)])
-        YieldDictComp = OrderedDict( [ ("Bin",b)])
-        for (p,e) in DC_Bkg.exp[b].items(): # so that we get only self.DC.processes contributing to this bin
-            if "DiLep" in p:
-                YieldDict["CR"] += e
-                
-
-        for (p,e) in DC_Bkg.exp[b].items(): # so that we get only self.DC.processes contributing to this bin
-            if DC_Bkg.isSignal[p]:
-                YieldDict["CR-Sig.Cont."]= e/YieldDict["CR"] if YieldDict["CR"]!=0 else -1
-
-        if DC_Bkg.obs[b]!=0: YieldDict["CR-DiLep.Pur."]= YieldDict["CR"]/DC_Bkg.obs[b]
-        else: YieldDict["CR-DiLep.Pur."]= -1
-        for repl in [#"_1B_",
-            "_2B_","_3p_"]:
-#            replacedBin = b.replace("_1p_",repl).replace("DPhi00","DPhi10")
-            replacedBin = b.replace("_1p_",repl).replace("DPhi00","DPhi05")
-#            replacedBin = b.replace("_1p_",repl).replace("DPhi00","Stop")
-#            replacedBin = b.replace("_1p_",repl).replace("DPhi00","Top")
-            if replacedBin in DC_Sig.list_of_bins():
-                for (p,e) in DC_Sig.exp[replacedBin].items(): # so that we get only self.DC.processes contributing to this bin
-                    if "DiLep" in p:
-                        YieldDict[repl.replace("_","")] += e
-                        YieldDict[repl.replace("_","")+"Rel"] = YieldDict["CR"]/e
-                        YieldDict[repl.replace("_","")+"BkgFrac"] = e/DC_Sig.obs[replacedBin]
-                        YieldDictComp["SR_"+p+"_"+repl.replace("_","")] = e
-                        
-                        
-
-            else:
-                YieldDict[repl.replace("_","")] = -1
-                YieldDict[repl.replace("_","")+"Rel"] = -1
-
-
-        for repl in [#"_1B_",
-            "_2B_","_3p_"]:
-            replacedBin = b.replace("_1p_",repl).replace("DPhi00","DPhi05")
-            if replacedBin in DC_Sig.list_of_bins():
-                for (p,e) in DC_Sig.exp[replacedBin].items(): # so that we get only self.DC.processes contributing to this bin
-                    if "DiLep" in p:
-#                        print p
-                        YieldDictComp["SR_"+p+"_"+repl.replace("_","")] = YieldDictComp["SR_"+p+"_"+repl.replace("_","")]/YieldDict[repl.replace("_","")]
-
-
-        CollectYieldDicts.append(YieldDict)
-        CollectYieldCompDicts.append(YieldDictComp)
-print 
-print tabulate(CollectYieldDicts, headers="keys", floatfmt=".4f")
-print tabulate(CollectYieldDicts, headers="keys", floatfmt=".4f", tablefmt="latex")
-print tabulate(CollectYieldCompDicts, headers="keys", floatfmt=".4f", tablefmt="latex")
-
+    print tabulate(CollectYieldDicts, headers="keys", floatfmt=".4f")
