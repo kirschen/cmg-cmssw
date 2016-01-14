93985b693515c12ad3b7fad0298a44f6ea5a92c4
Henning Kirschenmann <henning.kirschenmann@desy.de>
2016-01-14 09:55:42 +0100

    add weights to systematics module that were used for dilepton control region systematic in ANv3

--- CMGTools/SUSYAnalysis/python/tools/eventVars_1l_WeightsForSystematics.py
+++ CMGTools/SUSYAnalysis/python/tools/eventVars_1l_WeightsForSystematics.py
@@ -4,14 +4,27 @@ import ROOT
 
 class EventVars1LWeightsForSystematics:
     def __init__(self):
-        self.branches = ["GenTopPt", "GenAntiTopPt", "TopPtWeight", "GenTTBarPt", "GenTTBarWeight" 
+        self.branches = ["GenTopPt", "GenAntiTopPt", "TopPtWeight", "GenTTBarPt", "GenTTBarWeight", "DilepNJetWeightConst", "DilepNJetWeightSlope", "DilepNJetWeight", 
                          ]
 
     def listBranches(self):
         return self.branches[:]
 
-    def __call__(self,event,keyvals):
+    def __call__(self,event,base={}):
         #
+        # prepare output
+        ret = {}
+        for name in self.branches:
+#            print name
+            if type(name) is tuple:
+                ret[name] = []
+            elif type(name) is str:
+                ret[name] = -999.0
+            else:
+                print "could not identify"
+#        print ret
+
+        if base['Selected']!=1: return ret #only run the full module on selected leptons, not the ones for QCD estimate
 
         GenTopPt = -999
         GenTopIdx = -999
@@ -46,12 +59,43 @@ class EventVars1LWeightsForSystematics:
                     if GenTTBarPt>150: GenTTBarWeight= 0.90
                     if GenTTBarPt>250: GenTTBarWeight= 0.80
                     if GenTTBarPt>400: GenTTBarWeight= 0.70
+
 #markus proposal for njet-reweighting           
 #The level of agreement is important to us. We could reweight top events by 1.05 per jet that does not come from the top and compare again the distributions (changes yield ~ 27% for 5 jets not from the top). If that is more that the difference we see in data we can propose this as an uncertainty.
 
 
+        # values in sync with AN2015_207_v3
+        #        Const weight
+        # const: 0.85 +-0.06
+        #        16%
+        wmean = 5.82 - 0.5
+        # slope: 0.03 +/-0.05
+        slopevariation = sqrt(0.03*0.03 +0.05*0.05) 
+        if (event.ngenLep+event.ngenTau)==2:
+            ret['DilepNJetWeightConst'] = 0.84
+            ret['DilepNJetWeightSlope'] = 1+ (base['nJets30Clean']-wmean)*slopevariation
+        else:
+            ret['DilepNJetWeightConst'] = 1.
+            ret['DilepNJetWeightSlope'] = 1.
 
-        ret    =  { 'GenTopPt'   : GenTopPt } #initialize the dictionary with a first entry
+
+        #Alternative "all in one" weight which is the envelope of the 68% unc. band with a big jump around the mean value (to be deprecated)
+        DiLepWeightDict = dict([ ( 3 , 0.8402), 
+                                 ( 4 , 0.8913), 
+                                 ( 5 , 0.9265), 
+                                 ( 6 , 1.0788), 
+                                 ( 7 , 1.1286), 
+                                 ( 8 , 1.1832), 
+                                 ( 9 , 1.2475), 
+                                 ( 10 , 1.3034), 
+                                 ( 11 , 1.3578), 
+                                 ])
+        if base['nJets30Clean']<12 and base['nJets30Clean']>2 and event.ngenLep+event.ngenTau==2:
+            ret['DilepNJetWeight'] = DiLepWeightDict[base['nJets30Clean']]
+        else:
+            ret['DilepNJetWeight'] = 1.
+
+        ret['GenTopPt'] = GenTopPt
         ret['GenAntiTopPt'] = GenAntiTopPt
         ret['TopPtWeight']  = TopPtWeight
         ret['GenTTBarPt']  = GenTTBarPt
